db8bbc3e6b24a81562e615c91615c979c8feec10
Roysurabhi <surabhi.roy1509@gmail.com>
2025-10-28 21:28:42 +0000

    Add files via upload

---
+++ variables_intro.py
@@ -0,0 +1,14 @@
+name = "Shreya"
+age = 30
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is a Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
